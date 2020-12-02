17a795d1115d950c6f0ccc68f275eeccaa99a593
sudharsan004 <sudharsansriram8921@gmail.com>
2020-12-02 11:34:00 +0530

    setup template files

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'accounts/dashboard.html')
+
+def product(request):
+    return render(request,'accounts/product.html')
+
+def customer(request):
+    return render(request,'accounts/customer.html')
