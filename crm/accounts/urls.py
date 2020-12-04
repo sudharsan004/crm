858b8e37b375aaf6c75f7f6e1a6ed3b1e4f8cfa5
sudharsan004 <sudharsansriram8921@gmail.com>
2020-12-04 15:54:57 +0530

    render data in html pages

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.home, name ="home"),
-    path('product/',views.product, name ="product"),
-    path('customer/',views.customer, name ="customer"),
+    path('', views.home, name="home"),
+    path('product/', views.product, name="product"),
+    path('customer/<str:pk>/', views.customer, name="customer"),
 ]
--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -6,17 +6,38 @@ from .models import *
 def home(request):
     last_five_orders = Order.objects.filter().order_by('-id')[:5]
     last_five = reversed(last_five_orders)
-    orders = Order.objects.all()
+    total_order_count = Order.objects.all().count()
+    orders_delivered_count = Order.objects.filter(status="Delivered").count()
+    orders_pending_count = total_order_count-orders_delivered_count
     customer = Customer.objects.all()
-    context = {'orders': last_five, 'customers': customer}
+
+    context = {'orders': last_five, 'customers': customer,
+               'order_stats': {'total_orders': total_order_count,
+                               'orders_delivered': orders_delivered_count,
+                               'orders_pending': orders_pending_count
+                               }}
     return render(request, 'accounts/dashboard.html', context)
 
 
 def product(request):
     products = Product.objects.all()
-
     return render(request, 'accounts/product.html', {'products': products})
 
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    all_orders = customer.order_set.all()
+    try:
+        orders_delivered = customer.order_set.filter(
+            status="Delivered").count()
+    except Order.DoesNotExist:
+        orders_delivered = 0
+    try:
+        orders_pending = customer.order_set.filter(status="Pending").count()
+    except Order.DoesNotExist:
+        orders_pending = 0
+    orders_out_for_delivery = all_orders.count()-(orders_delivered+orders_pending)
+
+    context = {'customer': customer, 'all_orders': all_orders,
+               'orders_delivered': orders_delivered, 'orders_pending': orders_pending, 'orders_out_for_delivery': orders_out_for_delivery}
+    return render(request, 'accounts/customer.html', context=context)
